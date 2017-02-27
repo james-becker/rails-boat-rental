4efb7a901253114ba213de64f4b3f1393ce7a703
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-02-27 21:14:02 +0100

    fixed dob column to string, changed gemfile to request datepicker, did datepicker installation, changed devise/user_sign_in to include proper simple form for html. Datepicker now works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227170640) do
+ActiveRecord::Schema.define(version: 20170227200756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 20170227170640) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "address"
-    t.integer  "dob"
+    t.string   "dob"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
