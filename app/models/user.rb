d72c8c3ba3fcd6c929a7e7e6ddbe361280702b55
James Becker <jamsbec@gmail.com>
2017-02-27 19:48:58 +0100

    Update user model with dob

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :first_name, presence: :true
   validates :last_name, presence: :true
   validates :address, presence: :true
-  validates :age, presence: :true, numericality: {only_integer: :true}
+  validates :dob, presence: :true, numericality: {only_integer: :true}
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
