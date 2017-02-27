c9c3f337e797f8258b493f97bfdd3851293b5bc9
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-02-27 18:11:40 +0100

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :address, presence: true
   validates :age, presence: true, numericality: {only_integer: true}
-  validates :photo, presence: true;
+  validates :photo, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
