3f999a210df7a9327781d4c85afb92d1bdc6e7ef
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-03-01 17:04:30 +0100

    authenticate user only on correct pages

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,7 +1,5 @@
 class BoatsController < ApplicationController
-
-
-  skip_before_action :authenticate_user!, only: [:index,:search]
+  skip_before_action :authenticate_user!, only: [:index, :search, :show]
 
   before_action :set_boat, except: [:index, :new, :create, :search]
 
