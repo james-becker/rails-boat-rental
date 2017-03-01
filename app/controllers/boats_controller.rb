30693c40f44205cb69c71cda8662f13e752cc0a4
(Amanda Orbe) <garayomiriam@gmail.com>
2017-03-01 18:07:39 +0100

    whatever

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,7 +1,7 @@
 class BoatsController < ApplicationController
 
 
-  skip_before_action :authenticate_user!, only: [:index,:search]
+  #skip_before_action :authenticate_user!, only: [:index,:search]
 
   before_action :set_boat, except: [:index, :new, :create, :search]
 
