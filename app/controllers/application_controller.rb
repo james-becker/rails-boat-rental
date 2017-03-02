5a101390a60c610e32387ef3d02ea6412ec4c83b
juliette engel <juliette.engel@skema.edu>
2017-03-02 16:36:56 +0100

    Redirect back to current page after sign in, sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  after_filter :store_location
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob, :photo])
@@ -15,5 +17,18 @@ class ApplicationController < ActionController::Base
   def self.current_user
     @user = current_user
   end
+
+
+
+
+def store_location
+  # store last url as long as it isn't a /users path
+  session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+end
+
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
+
 
 end
