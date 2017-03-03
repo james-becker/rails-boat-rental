6b2bfdc7d28e620975f2c5af7157b7ca20c21963
James Becker <jamsbec@gmail.com>
2017-03-03 12:58:01 +0100

    updated stars logic in helper method

--- app/helpers/boats_helper.rb
+++ app/helpers/boats_helper.rb
@@ -1,2 +1,19 @@
 module BoatsHelper
+  def boat_stars(boat)
+    if !boat.reviews.average(:stars).nil?
+
+        total = (boat.reviews.average(:stars) * 2).round / 2.0
+
+        full = total - (total % 1)
+        half = total % 1
+        empty = 5 - full - half
+      return {
+        full: full.round,
+        half: half.round,
+        empty: empty.round
+      }
+    else
+      return nil
+    end
+  end
 end
